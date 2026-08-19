f8c9bd96506731790a936a8599865a898a27e3e8
dikahusniadhy-art <dika.husniadhy@krom.id>
2026-08-19 16:44:25 +0700

    Update input.js

--- js/input.js
+++ js/input.js
@@ -2,7 +2,7 @@
  *
  * DATA AGUNAN CUSTODY
  * FILE      : input.js
- * VERSION   : 2.0 FINAL
+ * VERSION   : 1.0 FINAL
  *
  ******************************************************************************/
 
